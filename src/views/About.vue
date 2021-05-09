aed3bb52901589c7195b5c4fbb40270e3dfc0147
frida stenberg <fridastenberg97@hotmail.com>
2021-05-10 00:05:06 +0200

    about

---
+++ src/views/About.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="about">
+    <h1>This is an about page:</h1>
+    <section>
+      <p>Da Eitha! Na Chuba negatorie Na! Na! Natoota... Boscka! I have come for the bounty on this Wookiee. Oh, no! Chewbacca! At last we have the mighty Chewbacca. Oh, uh, yes, uh, I am here, Your Worshipfulness. Uh... yes! Oh. The illustrious Jabba bids you welcome and will gladly pay you the reward of twenty-five thousand. I want fifty thousand. No less. Uh, oh... but what, what did I say? Uh, the mighty Jabba asks why he must pay fifty thousand. Because he's holding a thermal detonator. This bounty hunter is my kind of scum. Fearless and inventive. Jabba offers the sum of thirty-five. And I suggest you take it. Zeebuss. He agrees! Just relax for a moment.</p>
+
+      <p>Threepio, you tell that slimy piece of... worm-ridden filth he'll get no such pleasure from us. Right? Jabba! This is your last chance. Free us or die. Move him into position. Put him in.</p>
+
+      <p>Master Yoda... is Darth Vader my father? Mmm... rest I need. Yes... rest. Yoda, I must know. Your father he is. Told you, did he? Yes. Unexpected this is, and unfortunate... Unfortunate that I know the truth? No. Unfortunate that you rushed to face him... that incomplete was your training. Not ready for the burden were you. Well, I'm sorry. Remember, a Jedi's strength flows from the Force. But beware. Anger, fear, aggression. The dark side are they. Once you start down the dark path, forever will it dominate your destiny.</p>
+
+      <p>Your fleet has lost. And your friends on the Endor moon will not survive. There is no escape, my young apprentice. The Alliance will die...as will your friends. Good. I can feel your anger. I am defenseless. Take your weapon! Strike me down with all your hatred, and your journey towards the dark side will be complete.</p>
+
+      <p>Yes! I said closer! Move as close as you can and engage those Star Destroyers at point-blank range. At that close range, we won't last long against those Star Destroyers. We'll last longer then we will against that Death Star...and we might just take a few of them with us. She's gonna blow! I'm hit!</p>
+
+      <p>If I don't make it back, you're the only hope for the Alliance. Luke, don't talk that way. You have a power I--I don't understand and could never have. You're wrong, Leia. You have that power too. In time you'll learn to use it as I have. The Force is strong in my family. My father has it... I have it ...and...my sister has it. Yes. It's you Leia. I know. Somehow...I've always known. Then you know why I have to face him.</p>
+
+      <p>Easy, Chewie. Whoa! Whoa! Help! Chewie, you okay? Where is he? I'm okay, pal. Han! Chewie? Lando! Boba Fett?! Boba Fett?! Where? Lando, grab it! Lower it! I'm trying! Whoa! Whoa! Grab me, Chewie! I'm slipping. Grab it! L--Lando. Grab! Grab it! Almost... You almost got it! Hold it! Whoa! Gently now. All... all right. Now easy, easy. Hold me, Chewie. Chewie! Chewie, give me the gun. Don't move, Lando. No, wait! I thought you were blind! It's all right. Trust me. Don't move. All right! A little higher! Just a little higher! Chewie, Pull us up! Come on! Okay... up, Chewie, up!</p>
+
+      <p>Hmm. That face you make. Look I so old to young eyes? No... of course not. I do, yes, I do! Sick have I become. Old and weak. When nine hundred years old you reach, look as good you will not. Hmm? Soon will I rest. Yes, forever sleep. Earned it, I have. Master Yoda, you can't die. Strong am I with the Force... but not that strong! Twilight is upon me and soon night must fall. That is the way of things... the way of the Force. But I need your help. I've come back to complete the training. No more training do you require. Already know you that which you need. Then I am a Jedi? Ohhh. Not yet. One thing remains: Vader. You must confront Vader. Then, only then, a Jedi will you be. And confront him you will.</p>
+
+      <p>All right! Up! Move! Come on! Quickly! Quickly, Chewie. Han! Hurry! The fleet will be here any moment. Charges! Come on, come on! Oh, my! They'll be captured! Wa-wait! Wait, come back! Artoo, stay with me. Freeze! You Rebel scum.</p>
+
+      <p>Watch out. Squad at .06. I'm on it, Gold Leader. Good shot, Red Two. Now...come on, Han, old buddy. Don't let me down.</p>
+
+      <p>Yes, Artoo. I was just coming to that. What's going on? I don't know. Wonderful! We are now a part of the tribe. Just what I always wanted. Well, short help is better than no help at all, Chewie. Thank you. Okay. He says the scouts are going to show us the quickest way to the shield generator. Good. How far is it? Ask him. We need some fresh supplies, too. And try and get our weapons back. And hurry up, will ya? I haven't got all day.</p>
+
+      <p>No! Luke, run away, far away. If he can feel your presence, then leave this place. I wish I could go with you. No, you don't. You've always been strong. But, why must you confront him? Because...there is good in him. I've felt it. He won't turn me over to the Emperor. I can save him. I can turn him back to the good side. I have to try. Hey, what's goin' on? Nothing. I - just want to be alone for a little while. Nothing? Come on, tell me. What's goin' on? I...I can't tell you. Did you tell Luke? Is that who you could tell? I... Ahhh... I'm sorry. Hold me.</p>
+
+      <p>Lord Vader, this is an unexpected pleasure. We're honored by your presence. You may dispense with the pleasantries, Commander. I'm here to put you back on schedule. I assure you, Lord Vader, my men are working as fast as they can. Perhaps I can find new ways to motivate them. I tell you, this station will be operational as planned.</p>
+
+      <p>The Emperor knew, as I did, if Anakin were to have any offspring, they would be a threat to him. That is the reason why your sister remains safely anonymous. Leia! Leia's my sister. Your insight serves you well. Bury your feelings deep down, Luke. They do you credit. But they could be made to serve the Emperor.</p>
+
+      <p>Greetings, Exalted One. Allow me to introduce myself. I am Luke Skywalker, Jedi Knight and friend to Captain Solo. I know that you are powerful, mighty Jabba, and that your anger with Solo must be equally powerful. I seek an audience with Your Greatness to bargain for Solo's life. With your wisdom, I'm sure that we can work out an arrangement which will be mutually beneficial and enable us to avoid any unpleasant confrontation. As a token of my goodwill, I present to you a gift: these two droids. What did he say? Both are hardworking and will serve you well.</p>
+
+      <p>Oh, I told you it was dangerous here. Shall we try and go around? It'll take time. This whole party'll be for nothing if they see us. Chewie and I will take care of this. You stay here. Quietly, there might be more of them out there. Hey... it's me. Go for help! Go! Great. Come on. Over there! Two more of them! I see them. Wait, Leia! Quick! Jam their comlink. Center switch! Hey, wait! Ahhh! Move closer! Get alongside that one! Get him! Keep on that one! I'll take these two!</p>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+
+section {
+  padding-left: 4rem;
+  padding-right: 4rem;
+  padding-bottom: 4rem;
+}
+
+p {
+  color: white;
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+</style>
