ca164640f5628fab0a34056770cae0d6d85fc34c
frida stenberg <fridastenberg97@hotmail.com>
2021-05-10 00:06:19 +0200

    todoform

---
+++ src/components/todolist.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!--denna bara loopar igenom todos och sen skickar in en todo till todo.vue, det vi ser i webbläsaren är alltså inte denna, utan den andra filen-->
+    <todo v-for="(todo, i) in todos" :key="i" :todo="todo"/>
+  </div>
+</template>
+
+<script>
+import todo from "./todo.vue"
+export default {
+    components: {
+        todo
+    },
+    computed: {
+        todos(){
+            return this.$store.state.todos
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
