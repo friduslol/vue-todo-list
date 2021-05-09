e94de0ebd461ad4c51e15f881dd0ec62991e31b6
frida stenberg <fridastenberg97@hotmail.com>
2021-05-10 00:05:00 +0200

    add

---
+++ src/views/Add.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="add">
+      <h1>Add a Todo to your List:</h1>
+      <todoform />
+  </div>
+</template>
+
+<script>
+import todoform from "../components/todoform.vue";
+
+export default {
+    name: "Add",
+    components: {
+        todoform,
+    }
+}
+</script>
+
+<style scoped>
+</style>
+
