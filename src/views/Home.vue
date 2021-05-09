9349dc9ad57dfd963d0c2500b81e8e3b2bcfda60
frida stenberg <fridastenberg97@hotmail.com>
2021-05-10 00:04:52 +0200

    home

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>Todo List:</h1>
+    <todolist />
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import todolist from "../components/todolist.vue"
+
+export default {
+  name: 'Home',
+  components: {
+    todolist,
+
+
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
