6ef182eb392b3cbaa18924f552caec0bc3d5c3d6
frida stenberg <fridastenberg97@hotmail.com>
2021-05-10 00:07:06 +0200

    nabar

---
+++ src/components/navbar.vue
@@ -0,0 +1,37 @@
+<template>
+   <div class="navbar">
+    <router-link to="/">Home</router-link>
+    <router-link to="/add">Add</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.navbar {
+    background: #000000;
+    padding: 1rem;
+    text-align: center;
+}
+
+a {
+    color: #ffffff;
+    font-size: 1.5rem;
+    margin: 0 1rem;
+    text-decoration: none;
+    transition: 0.3s;
+    font-weight: bold;
+
+}
+
+a:hover {
+    color: #a02d2d;
+}
+
+</style>
